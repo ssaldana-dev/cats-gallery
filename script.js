704eae93220848f8c7abe8356b134053ee2f873e
Sergio Saldaña <ssaldana.dev@gmail.com>
2022-08-11 03:52:34 -0500

    created the kitties array in JS and saved 5 objects on it that correspond to each kittie photo, description, author and alt message

---
+++ script.js
@@ -0,0 +1,32 @@
+const kitties = [];
+
+kitties.push({
+    src: './assets/gatos-1',
+    description: 'Este gatito se ve muy feliz, miren nada más sus ojitos cerratos y sus patitas hacia arriba.',
+    author: 'Pixabay',
+    alt: 'Gatito, Acostado, En, Superficie'
+});
+kitties.push({
+    src: './assets/gatos-2',
+    description: 'Este gatito se ve súper tierno mirando hacia arriba, parece ser una descripción gráfica de la curiosidad de los gatos.',
+    author: 'Wojciech Kumpicki',
+    alt: 'Gato Atigrado Naranja Cerca De La Ventana'
+});
+kitties.push({
+    src: './assets/gatos-3',
+    description: 'Estos dos gatitos parecen estar divirtiéndose con un juguete colgante. Amo cuando hacens esas cosas.',
+    author: 'Pixabay',
+    alt: 'Gatitos Atigrados En Edredón Floral'
+});
+kitties.push({
+    src: './assets/gatos-4',
+    description: 'Este gatito aparte de tierno parece estar posando al más puro estilo de Latrell Spencer.',
+    author: 'Anel Rossouw',
+    alt: 'Foto De Gatito Atigrado Gris Acostado'
+});
+kitties.push({
+    src: './assets/gatos-5',
+    description: 'Qué más decir de este gatito, su rostro plácido y sereno es suficiente para disfrutar de esta fotografía.',
+    author: 'Ihsan Adityawarman',
+    alt: 'Fotografía En Primer Plano De Gato Atigrado Durmiendo'
+});
