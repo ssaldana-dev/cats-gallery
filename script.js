9519e652a6a243483dd98f5b65ab3beedc8c29f1
Sergio Saldaña <ssaldana.dev@gmail.com>
2022-08-11 04:06:30 -0500

    Added the authors site to the photos information on the objects from kitties array

--- script.js
+++ script.js
@@ -4,29 +4,34 @@ kitties.push({
     src: './assets/gatos-1',
     description: 'Este gatito se ve muy feliz, miren nada más sus ojitos cerratos y sus patitas hacia arriba.',
     author: 'Pixabay',
+    authorSite: 'https://www.pexels.com/@pixabay/',
     alt: 'Gatito, Acostado, En, Superficie'
 });
 kitties.push({
     src: './assets/gatos-2',
     description: 'Este gatito se ve súper tierno mirando hacia arriba, parece ser una descripción gráfica de la curiosidad de los gatos.',
     author: 'Wojciech Kumpicki',
+    authorSite: 'https://www.pexels.com/@wojciech-kumpicki-1084687/',
     alt: 'Gato Atigrado Naranja Cerca De La Ventana'
 });
 kitties.push({
     src: './assets/gatos-3',
     description: 'Estos dos gatitos parecen estar divirtiéndose con un juguete colgante. Amo cuando hacens esas cosas.',
     author: 'Pixabay',
+    authorSite: 'https://www.pexels.com/@pixabay/',
     alt: 'Gatitos Atigrados En Edredón Floral'
 });
 kitties.push({
     src: './assets/gatos-4',
     description: 'Este gatito aparte de tierno parece estar posando al más puro estilo de Latrell Spencer.',
     author: 'Anel Rossouw',
+    authorSite: 'https://www.pexels.com/@anelrossouw/',
     alt: 'Foto De Gatito Atigrado Gris Acostado'
 });
 kitties.push({
     src: './assets/gatos-5',
     description: 'Qué más decir de este gatito, su rostro plácido y sereno es suficiente para disfrutar de esta fotografía.',
     author: 'Ihsan Adityawarman',
+    authorSite: 'https://www.pexels.com/@ihsanaditya/',
     alt: 'Fotografía En Primer Plano De Gato Atigrado Durmiendo'
 });
