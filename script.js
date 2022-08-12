d276fd66774460ba51e96b9d708d94fb7fe1b403
Sergio Saldaña <ssaldana.dev@gmail.com>
2022-08-12 04:23:39 -0500

    Deleted the images from the repository and uploaded in another server, also referenced the routes at the script.js

--- script.js
+++ script.js
@@ -8,35 +8,35 @@ const buttonRight = document.querySelector('#right-button');
 const kitties = [];
 
 kitties[1] = {
-    src: './assets/gatos-1.jpg',
+    src: 'https://i.imgur.com/d2eIISv.jpg',
     description: 'Este gatito se ve muy feliz, miren nada más sus ojitos cerraditos y sus patitas hacia arriba.',
     author: 'Pixabay',
     authorSite: 'https://www.pexels.com/@pixabay/',
     alt: 'Gatito, Acostado, En, Superficie'
 };
 kitties[2] = {
-    src: './assets/gatos-2.jpg',
+    src: 'https://i.imgur.com/GJbSmUJ.jpg',
     description: 'Este gatito en su instinto por esconderse nos regaló una de las fotos más bonitas.',
     author: 'Francesco Ungaro',
     authorSite: 'https://www.pexels.com/@francesco-ungaro/',
     alt: 'Gato Atigrado Naranja Cerca De La Ventana'
 };
 kitties[3]= {
-    src: './assets/gatos-3.jpg',
+    src: 'https://i.imgur.com/S2zOnV6.jpg',
     description: 'Estos dos gatitos parecen estar divirtiéndose con un juguete colgante. Amo cuando hacens esas cosas.',
     author: 'Pixabay',
     authorSite: 'https://www.pexels.com/@pixabay/',
     alt: 'Gatitos Atigrados En Edredón Floral'
 };
 kitties[4] = {
-    src: './assets/gatos-4.jpg',
+    src: 'https://i.imgur.com/L9dFzJT.jpg',
     description: 'Este gatito aparte de tierno parece estar posando al más puro estilo de Latrell Spencer.',
     author: 'Anel Rossouw',
     authorSite: 'https://www.pexels.com/@anelrossouw/',
     alt: 'Foto De Gatito Atigrado Gris Acostado'
 };
 kitties[5] = {
-    src: './assets/gatos-5.jpg',
+    src: 'https://i.imgur.com/c3Pg72P.jpg',
     description: 'Qué más decir de este gatito, su rostro plácido y sereno es suficiente para disfrutar de esta fotografía.',
     author: 'Ihsan Adityawarman',
     authorSite: 'https://www.pexels.com/@ihsanaditya/',
